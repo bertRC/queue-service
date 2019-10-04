9d58e2e9abb828363bd734cf8783812c21cae302
bertRC <sithalbert@ya.ru>
2019-10-04 17:13:46 +0300

    метод getFirst временно закоментирован

--- src/main/java/ru/itpark/service/QueueService.java
+++ src/main/java/ru/itpark/service/QueueService.java
@@ -3,9 +3,9 @@ package ru.itpark.service;
 public class QueueService {
     private QueueElement first;
 
-    public QueueElement getFirst() {
-        return first;
-    }
+//    public QueueElement getFirst() {
+//        return first;
+//    }
 
     public void setFirst(QueueElement first) {
         this.first = first;
