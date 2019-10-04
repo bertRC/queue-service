52c0d4603a5e245f6276cbed92a57a39a5285779
bertRC <sithalbert@ya.ru>
2019-10-04 17:01:04 +0300

    Добавлен класс QueueService

---
+++ src/main/java/ru/itpark/service/QueueService.java
@@ -0,0 +1,13 @@
+package ru.itpark.service;
+
+public class QueueService {
+    private QueueElement first;
+
+    public QueueElement getFirst() {
+        return first;
+    }
+
+    public void setFirst(QueueElement first) {
+        this.first = first;
+    }
+}
